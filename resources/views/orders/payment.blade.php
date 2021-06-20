5264d9449d7997bc82b34fb6a98c99707fbf95f6
Sebastian Maciel <Sebamaciel15@gmail.com>
2021-06-20 20:18:36 -0300

    integrating MercadoPago sdk

--- resources/views/orders/payment.blade.php
+++ resources/views/orders/payment.blade.php
@@ -29,6 +29,64 @@
                 </div>
             </div>
         </div>
+
+        <div class="bg-white rounded-lg shadow-lg p-6 text-gray-700 mb-6">
+            <p class="text-xl font-semibold mb-4">Resumen</p>
+
+            <table class="table-auto w-full">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th>Total</th>
+                    </tr>
+                </thead>
+
+                <tbody class="divide-y divide-gray-200">
+                    @foreach ($items as $item)
+                        <tr>
+                            <td>
+                                <div class="flex">
+                                    <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}" alt="">
+                                    <article>
+                                        <h1 class="font-bold">{{ $item->name }}</h1>
+                                        <div class="flex text-xs">
+                                            @isset($item->options->color)
+                                                Color: {{ __($item->options->color) }}
+                                            @endisset
+
+                                            @isset($item->options->size)
+                                                - {{ $item->options->size }}
+                                            @endisset
+                                        </div>
+                                    </article>
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                {{ $item->price }} USD
+                            </td>
+                            <td class="text-center">
+                                {{ $item->qty }}
+                            </td>
+                            <td class="text-center">
+                                {{ $item->price * $item->qty }} USD
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        <div class="bg-white rounded-lg shadow-lg p-6 flex justify-between items-center">
+            <img class="h-12" src="{{ asset('img/tarjeta-de-débito.jpg') }}" alt="">
+            <div class="text-gray-700">
+                <p class="text-sm font-semibold">Subtotal: {{ $order->total - $order->shipping_cost }} USD
+                </p>
+                <p class="text-sm font-semibold">Envío: {{ $order->shipping_cost }} USD</p>
+                <p class="text-lg font-semibold uppercase">Total: {{ $order->total }} USD</p>
+            </div>
+        </div>
     </div>
 
 </x-app-layout>
