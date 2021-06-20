977851dfba8096e68ae51f00c0394aaad52cb7f5
Sebastian Maciel <Sebamaciel15@gmail.com>
2021-06-20 18:14:55 -0300

    creating view order.payment at ML

--- resources/views/orders/payment.blade.php
+++ resources/views/orders/payment.blade.php
@@ -1,3 +1,34 @@
 <x-app-layout>
+    <div class="container py-8">
+        <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
+            <p class="text-gray-700 uppercase"><span class="font-semibold">Número de orden:</span>
+                Orden-{{ $order->id }}</p>
+        </div>
+
+        <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
+            <div class="grid grid-cols-2 gap-6 text-gray-700">
+                <div>
+                    <p class="text-lg font-semibold uppercase">Envío</p>
+                    @if ($order->envio_type == 1)
+                        <p class="text-sm">Los productos deben ser retirados en tienda</p>
+                        <p class="text-sm">Calle falsa 123</p>
+                    @else
+                        <p class="text-sm">Los productos serán enviados a:</p>
+                        <p class="text-sm">{{ $order->address }}</p>
+                        <p>{{ $order->department->name }} - {{ $order->city->name }} -
+                            {{ $order->district->name }}
+                        </p>
+                    @endif
+                </div>
+
+                <div>
+                    <p class="text-lg font-semibold uppercase">Datos de contacto</p>
+
+                    <p class="text-sm">Persona que recibirá el producto: {{ $order->contact }}</p>
+                    <p class="text-sm">Teléfono de contacto: {{ $order->phone }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </x-app-layout>
